8fd11b082c9171f972f8bdd1a3086c916ec8a8b8
Anton Vlasenko <vlasenko.anton@gmail.com>
2020-08-20 16:50:20 +0200

    Add a config subscriber test.

--- app/bundles/LeadBundle/EventListener/ConfigSubscriber.php
+++ app/bundles/LeadBundle/EventListener/ConfigSubscriber.php
@@ -59,6 +59,7 @@ class ConfigSubscriber implements EventSubscriberInterface
         ]);
 
         $event->addForm([
+            'bundle'     => 'LeadBundle',
             'formAlias'  => 'segment_config',
             'formType'   => SegmentConfigType::class,
             'formTheme'  => 'MauticLeadBundle:FormTheme\Config',
