775c75612d060042720511437ae94a4cb1bb7482
John Linhart <admin@escope.cz>
2022-02-09 15:03:55 +0100

    Type fixes

--- app/bundles/LeadBundle/EventListener/TypeOperatorSubscriber.php
+++ app/bundles/LeadBundle/EventListener/TypeOperatorSubscriber.php
@@ -24,8 +24,8 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 final class TypeOperatorSubscriber implements EventSubscriberInterface
 {
