6ee3489cf7ee9a9c60348a5468e3f63db1b82601
Zdeno Kuzmany <zdeno@kuzmany.biz>
2022-02-16 13:01:49 +0100

    Update app/bundles/CoreBundle/Controller/CommonController.php
    Co-authored-by: John Linhart <jan@linhart.email>

--- app/bundles/CoreBundle/Controller/CommonController.php
+++ app/bundles/CoreBundle/Controller/CommonController.php
@@ -762,10 +762,7 @@ class CommonController extends Controller implements MauticController
         return $data->getDataForExport($start, $model, $args, $resultsCallback);
     }
 
-    /**
-     * @return string
-     */
-    protected function getDefaultOrderDirection()
+    protected function getDefaultOrderDirection(): string
     {
         return 'ASC';
     }
